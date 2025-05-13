be3466d3124bcfda4bb03fe8a02135bf9b97f10e
Filipe de Leonel Batista <filipe.x@live.com>
2025-05-12 23:26:21 -0300

    ajusts on week 1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,13 +1,4 @@
 const yearEl = document.getElementById('currentYear');
 if (yearEl) { yearEl.textContent = new Date().getFullYear(); }
 const lastEl = document.getElementById('lastModified');
-if (lastEl) { lastEl.textContent = `Last Modification: ${new Date(document.lastModified).toLocaleString('en-GB')}`; }
-
-const yearSpan = document.getElementById('currentyear');
-if (yearSpan) {
-    yearSpan.textContent = new Date().getFullYear();
-}
-const lastMod = document.getElementById('lastModified');
-if (lastMod) {
-    lastMod.textContent = `Last Modified: ${document.lastModified}`;
-}
+if (lastEl) { lastEl.textContent = `Last Modification: ${document.lastModified}`; }
